30b8af90ed568c3d24ac8bb95d2cbc9a1b9474ee
werton <mr.werton@gmail.com>
2023-06-22 00:45:16 -0300

    feat: started and configured

---
+++ src/main/java/com/notrew/tothink/TothinkApplication.java
@@ -0,0 +1,24 @@
+package com.notrew.tothink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class TothinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TothinkApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> get(){
+        return ResponseEntity.ok().body("hello");
+    };
+
+}
---
+++ src/test/java/com/notrew/tothink/TothinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notrew.tothink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TothinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
